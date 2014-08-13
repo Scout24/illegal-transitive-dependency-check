38e5b1eecefa0991c0a6479ceca699aa2b3d77f8
aschubert <andre.schubert@immobilienscout24.de>
2014-08-13 11:37:56 +0200

    Update to ASM5 to allow analysis of Java 8 code.

--- src/test/java/de/is24/maven/enforcer/rules/testtypes/ClassInDirectDependency.java
+++ src/test/java/de/is24/maven/enforcer/rules/testtypes/ClassInDirectDependency.java
@@ -1,6 +1,9 @@
 package de.is24.maven.enforcer.rules.testtypes;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ClassInDirectDependency {
   public enum EnumInClassInDirectDependency {
     ONE,
@@ -9,6 +12,10 @@ public class ClassInDirectDependency {
   }
 
   public EnumInClassInDirectDependency getOne() {
-    return EnumInClassInDirectDependency.ONE;
+      final List<String> list = Arrays.asList("1", "2", "3");
+      list.forEach(element -> Integer.parseInt(element));
+      long countOnes = list.stream().filter(element -> "1".equals(element)).count();
+
+      return EnumInClassInDirectDependency.ONE;
   }
 }
