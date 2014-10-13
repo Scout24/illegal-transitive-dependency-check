e40b9e9fa901a339a9facc5f8134537c8719651a
2bad4u <2bad4u@scubizone.de>
2014-10-13 20:11:31 +0200

    Extend test code.

--- src/test/java/de/is24/maven/enforcer/rules/ArtifactRepositoryAnalyzerTest.java
+++ src/test/java/de/is24/maven/enforcer/rules/ArtifactRepositoryAnalyzerTest.java
@@ -3,11 +3,14 @@ package de.is24.maven.enforcer.rules;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.testing.stubs.ArtifactStub;
 import org.junit.Test;
+
 import java.io.File;
 import java.util.Collections;
+
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 
 public class ArtifactRepositoryAnalyzerTest {
@@ -46,6 +49,52 @@ public class ArtifactRepositoryAnalyzerTest {
     assertThat(repository.getTypes().isEmpty(), is(true));
     assertThat(repository.getDependencies().isEmpty(), is(true));
     assertThat(logger.getInfoLog(), containsString("pom.xml', is skipped"));
+  }
+
+  @Test
+  public void invalidJarArtifact() {
+    final LogStub logger = new LogStub();
+    final ArtifactRepositoryAnalyzer analyzer = ArtifactRepositoryAnalyzer.analyzeArtifacts(
+        logger,
+        false,
+        false);
+
+    final Artifact artifact = makeArtifact(new File("invalid.jar"));
+
+    final String expectedErrorMessage = "Unable to read class(es) from artifact 'invalid.jar'.";
+    try {
+      analyzer.analyzeArtifacts(Collections.singleton(artifact));
+      fail("IllegalStateException expected!");
+    } catch (IllegalStateException e) {
+      assertThat(e.getMessage(), is(expectedErrorMessage));
+    }
+
+    assertThat(logger.getErrorLog(), containsString(expectedErrorMessage));
+  }
+
+  @Test
+  public void readTypesFromClassDirectory() {
+    final LogStub logger = new LogStub();
+    final ArtifactRepositoryAnalyzer analyzer = ArtifactRepositoryAnalyzer.analyzeArtifacts(
+        logger,
+        false,
+        false);
+
+    final File classFile = getCurrentClassFile();
+    final File classesDirectory = new File(classFile.getParent());
+    final Artifact artifact = makeArtifact(classesDirectory);
+
+    final Repository repository = analyzer.analyzeArtifacts(
+        Collections.singleton(artifact));
+
+    assertThat(repository.getTypes().isEmpty(), is(false));
+    assertThat(repository.getDependencies().isEmpty(), is(true));
+    assertThat(logger.getDebugLog(), containsString(classFile.getPath()));
+  }
+
+  private File getCurrentClassFile() {
+    final String resourcePath = "/" + ArtifactRepositoryAnalyzerTest.class.getName().replace(".", "/") + ".class";
+    return new File(ArtifactRepositoryAnalyzerTest.class.getResource(resourcePath).getFile());
   }
 
   private Artifact makeArtifact(File file) {
